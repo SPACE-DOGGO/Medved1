1ce322c3245e9cf13d23d1869e2eef5dc6f290d9
User <User@ADMIN>
2022-04-15 18:39:57 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,31 @@
+﻿// ConsoleApplication1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{   
+    system("color 1E");
+    cout << "                                                  \n";
+    cout << "                                                  \n";
+    cout << "     *     *                                      \n";
+    cout << "                                                  \n";
+    cout << "              *                                   \n";
+    cout << "                    *                             \n";
+    cout << "                         *           *            \n";
+    cout << "                                                  \n";
+    cout << "                                                  \n";
+    cout << "                          *        *              \n";
+    cout << "                                                  \n";
+    cout << "                                                  \n";
+    cout << "                                                  \n";
+    cout << "                                                  \n";
+    cout << "                                                  \n";
+    cout << "                                                  \n";
+    cout << "                                                  \n";
+    cout << "                                                  \n";
+    cout << "                                                  \n";
+    cout << "                                                  \n";
+    cout << "                                                  \n";
+}
